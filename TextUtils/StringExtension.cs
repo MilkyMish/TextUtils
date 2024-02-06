ce203e665c833d5804652aa7600dc94a512045a4
Mikhail Medvedev <m_medvedev@head-point.ru>
2024-02-06 18:00:07 +0300

    init

---
+++ TextUtils/StringExtension.cs
@@ -0,0 +1,37 @@
+﻿namespace TextUtils;
+
+public static class StringExtension
+{
+    public static string ToCamelCase(this string s)
+    {
+        if (string.IsNullOrEmpty(s) || !char.IsUpper(s[0]))
+        {
+            return s;
+        }
+
+        var chars = s.ToCharArray();
+
+        for (var i = 0; i < chars.Length; i++)
+        {
+            if (i == 1 && !char.IsUpper(chars[i]))
+            {
+                break;
+            }
+
+            var hasNext = (i + 1 < chars.Length);
+            if (i > 0 && hasNext && !char.IsUpper(chars[i + 1]))
+            {
+                if (char.IsSeparator(chars[i + 1]))
+                {
+                    chars[i] = char.ToLowerInvariant(chars[i]);
+                }
+
+                break;
+            }
+
+            chars[i] = char.ToLowerInvariant(chars[i]);
+        }
+
+        return new string(chars);
+    }
+}
